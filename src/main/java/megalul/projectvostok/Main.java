aa37183c49c994a94cf70aa77fa710fb039d440c
BuildTools <unconfigured@null.spigotmc.org>
2023-03-02 21:54:36 +0300

    push project

--- src/main/java/megalul/projectvostok/Main.java
+++ src/main/java/megalul/projectvostok/Main.java
@@ -1,9 +1,32 @@
 package megalul.projectvostok;
 
-public class Main{
+import glit.Glit;
+import glit.context.ContextListener;
+
+public class Main implements ContextListener{
 
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        Glit.create("Project Vostok", 1280, 720);
+        Glit.init(new Main());
+    }
+
+
+
+
+    public void init(){
+
+    }
+
+    public void render(){
+
+    }
+
+    public void resize(int i, int i1){
+
+    }
+
+    public void dispose(){
+
     }
 
 }
