ee144e95418158e25838efd39ddb3d1707b2bea0
BuildTools <unconfigured@null.spigotmc.org>
2023-03-04 16:36:13 +0300

    render texture

--- src/main/java/megalul/projectvostok/Main.java
+++ src/main/java/megalul/projectvostok/Main.java
@@ -2,6 +2,10 @@ package megalul.projectvostok;
 
 import glit.Glit;
 import glit.context.ContextListener;
+import glit.graphics.texture.Texture;
+import glit.graphics.util.Gl;
+import glit.graphics.util.TextureBatch;
+import glit.io.glfw.Key;
 
 public class Main implements ContextListener{
 
@@ -11,20 +15,30 @@ public class Main implements ContextListener{
     }
 
 
-    public void init(){
+    private TextureBatch batch;
+    private Texture texture;
 
+    public void init(){
+        batch = new TextureBatch();
+        texture = new Texture("textures/blocks/dirt.png");
     }
 
     public void render(){
+        Gl.clearColor(0.2, 0.15, 0.3);
+        Gl.clearBufferColor();
 
+        batch.begin();
+        batch.draw(texture, 100, 100, 600, 600);
+        batch.end();
+
+        if(Glit.isDown(Key.ESCAPE))
+            Glit.exit();
     }
 
-    public void resize(int i, int i1){
-
-    }
+    public void resize(int i, int i1){ }
 
     public void dispose(){
-
+        texture.dispose();
     }
 
 }
---
+++ src/main/java/megalul/projectvostok/game/World.java
@@ -0,0 +1,7 @@
+package megalul.projectvostok.game;
+
+public class World{
+
+
+
+}
